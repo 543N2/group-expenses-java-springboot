3a82a62d890194ec7648ca418e25c4875335ffc8
543n2 <543n2.one@gmail.com>
2022-04-23 20:59:47 -0500

    First commit

---
+++ src/main/java/com/jf/groupexpenses/GroupExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.jf.groupexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jf/groupexpenses/GroupExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jf.groupexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
